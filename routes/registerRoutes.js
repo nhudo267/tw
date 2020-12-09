071c3ffc80de738e77b632fe4aa66f6854d4480f
theanhnguyen02 <theanhnguyen02@gmail.com>
2020-12-09 23:36:43 +0700

    mm

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,3 +1,23 @@
+registerRoutes.js
+
+Loại
+JavaScript
+Kích thước
+2 KB (1.936 byte)
+Bộ nhớ chiếm dụng
+2 KB (1.936 byte)
+Vị trí
+routes
+Chủ sở hữu
+tôi
+Lần sửa đổi gần đây nhất:
+15 thg 10, 2020 bởi tôi
+Được mở vào
+23:31 bởi tôi
+Đã tạo
+22:48 với Google Drive Web
+Thêm mô tả
+Người xem có thể tải xuống
 const express = require('express');
 const app = express();
 const router = express.Router();
@@ -5,6 +25,8 @@ const bodyParser = require("body-parser")
 const bcrypt = require("bcrypt");
 const User = require('../schemas/UserSchema');
 
+app.set("view engine", "pug");
+app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -12,3 +34,56 @@ router.get("/", (req, res, next) => {
 
     res.status(200).render("register");
 })
+
+router.post("/", async (req, res, next) => {
+
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
+    var email = req.body.email.trim();
+    var password = req.body.password;
+
+    var payload = req.body;
+
+    if(firstName && lastName && username && email && password) {
+        var user = await User.findOne({
+            $or: [
+                { username: username },
+                { email: email }
+            ]
+        })
+        .catch((error) => {
+            console.log(error);
+            payload.errorMessage = "Something went wrong.";
+            res.status(200).render("register", payload);
+        });
+
+        if(user == null) {
+            // No user found
+            var data = req.body;
+            data.password = await bcrypt.hash(password, 10);
+
+            User.create(data)
+            .then((user) => {
+                req.session.user = user;
+                return res.redirect("/");
+            })
+        }
+        else {
+            // User found
+            if (email == user.email) {
+                payload.errorMessage = "Email already in use.";
+            }
+            else {
+                payload.errorMessage = "Username already in use.";
+            }
+            res.status(200).render("register", payload);
+        }
+    }
+    else {
+        payload.errorMessage = "Make sure each field has a valid value.";
+        res.status(200).render("register", payload);
+    }
+})
+
+module.exports = router;

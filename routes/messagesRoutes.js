d1d341dc2a700d4ab622da837a280c43d8b8d324
theanhnguyen02 <theanhnguyen02@gmail.com>
2020-12-09 23:40:46 +0700

    mm

---
+++ routes/messagesRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser")
+const bcrypt = require("bcrypt");
+const mongoose = require('mongoose');
+const User = require('../schemas/UserSchema');
+const Chat = require('../schemas/ChatSchema');
+
+router.get("/", (req, res, next) => {
+    res.status(200).render("inboxPage", {
+        pageTitle: "Inbox",
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user)
+    });
+})

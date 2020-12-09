5cafd32204c836cab7cb7100bf4910da1a42a426
chientran1461998 <62126686+chientran1461998@users.noreply.github.com>
2020-12-10 00:26:35 +0700

    Update database.js

--- database.js
+++ database.js
@@ -4,7 +4,11 @@
 //On subsequent calls, it will return the same instance that was 
 //created and returned to you the first time because of how module 
 //import/export works in ES6.
-
+const mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useUnifiedTopology', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useUnifiedTopology', true);
 
 class Database {
 

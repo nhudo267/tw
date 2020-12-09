b93fc152e6b8c74326443157f2cf363afdc0ba98
nhudo267 <nhudo164@gmail.com>
2020-12-09 22:34:08 +0700

    update

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 3003;
+const middleware = require('./middleware')
+const path = require('path')
+const bodyParser = require("body-parser")
+const mongoose = require("./database");
+const session = require("express-session");
+
+const server = app.listen(port, () => console.log("Server listening on port " + port));
+const io = require("socket.io")(server, { pingTimeout: 60000 });
+
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+
+    var payload = {
+        pageTitle: "Home",
+        userLoggedIn: req.session.user,
+        userLoggedInJs: JSON.stringify(req.session.user),
+    }
+
+    res.status(200).render("home", payload);
+})

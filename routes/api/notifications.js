09537096dfbcaa6f6f05cf6f8088b3f620662598
chientran1461998 <62126686+chientran1461998@users.noreply.github.com>
2020-12-10 00:04:59 +0700

    Create notifications.js

---
+++ routes/api/notifications.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const bodyParser = require("body-parser")
+const User = require('../../schemas/UserSchema');
+const Post = require('../../schemas/PostSchema');
+const Chat = require('../../schemas/ChatSchema');
+const Message = require('../../schemas/MessageSchema');
+const Notification = require('../../schemas/NotificationSchema');
+
+app.use(bodyParser.urlencoded({ extended: false }));

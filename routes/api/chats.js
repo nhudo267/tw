8d67c206bcb61d9d4e74e16b8763f52e9252d210
Nguyentuonghien <hientuong10101997@gmail.com>
2020-12-10 08:47:44 +0700

    nm

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -67,7 +67,14 @@ router.get("/:chatId", async (req, res, next) => {
     })
 })
 
-
+router.put("/:chatId", async (req, res, next) => {
+    Chat.findByIdAndUpdate(req.params.chatId, req.body)
+    .then(results => res.sendStatus(204))
+    .catch(error => {
+        console.log(error);
+        res.sendStatus(400);
+    })
+})
 
 router.get("/:chatId/messages", async (req, res, next) => {
     

a905c6ca8a9d87a7c09495105e3d1120ef790859
lehieu2898 <justinlee2898@gmail.com>
2020-12-10 08:41:56 +0700

    mm

--- routes/api/chats.js
+++ routes/api/chats.js
@@ -37,25 +37,7 @@ router.post("/", async (req, res, next) => {
     })
 })
 
-router.get("/", async (req, res, next) => {
-    Chat.find({ users: { $elemMatch: { $eq: req.session.user._id } }})
-    .populate("users")
-    .populate("latestMessage")
-    .sort({ updatedAt: -1 })
-    .then(async results => {
 
-        if(req.query.unreadOnly !== undefined && req.query.unreadOnly == "true") {
-            results = results.filter(r => r.latestMessage && !r.latestMessage.readBy.includes(req.session.user._id));
-        }
-
-        results = await User.populate(results, { path: "latestMessage.sender" });
-        res.status(200).send(results)
-    })
-    .catch(error => {
-        console.log(error);
-        res.sendStatus(400);
-    })
-})
 
 router.get("/:chatId", async (req, res, next) => {
     Chat.findOne({ _id: req.params.chatId, users: { $elemMatch: { $eq: req.session.user._id } }})
